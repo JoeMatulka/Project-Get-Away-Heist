4c1797511ddb6caf5e38ef608302ab96ebba829f
joematulka <78u980ZZ>
2019-07-01 21:07:01 -0500

    Added PlayerVehicle.cs

---
+++ Assets/Vehicles/Player/PlayerVehicle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerVehicle: Vehicle
+{
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        col = GetComponent<BoxCollider>();
+    }
+
+    void Update()
+    {
+        Move(input);
+    }
+}
--- Assets/Vehicles/Vehicle.cs
+++ Assets/Vehicles/Vehicle.cs
@@ -2,26 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
-[RequireComponent(typeof(BoxCollider))]
 public abstract class Vehicle : MonoBehaviour
 {
-    private Rigidbody rigidbody;
+    protected Rigidbody rigidbody;
 
-    private BoxCollider col;
-
-    private Vector3 input;
-
-    void Start()
-    {
-        rigidbody = GetComponent<Rigidbody>();
-        col = GetComponent<BoxCollider>();
-    }
-
-    void Update()
-    {
-        Move(input);
-    }
+    protected BoxCollider col;
 
     private void Move(Vector3 move) {
 
