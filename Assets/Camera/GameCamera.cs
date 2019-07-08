59e74090b8a8677be0eaf6f8a69739438a9e3bac
joematulka <78u980ZZ>
2019-07-08 16:59:16 -0500

    Replaced grid test material

--- Assets/Camera/GameCamera.cs
+++ Assets/Camera/GameCamera.cs
@@ -6,14 +6,14 @@ public class GameCamera : MonoBehaviour
 {
     public Transform target;
 
-    public float smoothSpeed = 0.125f;
+    public float smoothSpeed = .5f;
     public Vector3 offset;
 
     void FixedUpdate()
     {
         Vector3 desiredPosition = target.position + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-        transform.position = smoothedPosition;
+        transform.position = desiredPosition;
 
         transform.LookAt(target);
     }
