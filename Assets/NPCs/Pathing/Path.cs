0b27538c470208c1e646b0425616836f7c45c8bc
joematulka <78u980ZZ>
2019-10-21 20:32:24 -0500

    Renaming AIpath to path

--- Assets/NPCs/Pathing/Path.cs
+++ Assets/NPCs/Pathing/Path.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AIPath : MonoBehaviour
+public class Path : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
