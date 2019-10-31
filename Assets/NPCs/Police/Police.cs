389fa8de550ad01bc1fb34d2d9ac3e30f84eebcd
joematulka <78u980ZZ>
2019-10-31 16:40:39 -0500

    Added intial police script

--- Assets/NPCs/Police/Police.cs
+++ Assets/NPCs/Police/Police.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class Police : MonoBehaviour
 {
+    private Player player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
     }
 
     // Update is called once per frame
