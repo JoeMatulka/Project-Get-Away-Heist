536493a330be4748f8b44cce3abc4828c7f1294b
Joe Matulka <josef_matulka@hotmail.com>
2020-03-16 18:09:06 -0500

    Started work on spawning civilians along npc path

---
+++ Assets/NPCs/Civilian/CivilianManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CivilianManager
+{
+    public enum CivilianSpawnRate
+    {
+        // Numbers associated are the multipliers for generating civilians
+        NONE = 0,
+        SPARSE = 1,
+        NORMAL = 2,
+        DENSE = 3,
+    }
+    public class CivilianSpawner : Singleton<CivilianSpawner>
+    {
+        private static int CIV_SPAWN_MOD = 2;
+
+        public IEnumerator SpawnVehilcesOnPath(Path path, CivilianSpawnRate rate)
+        {
+            // Calculate the amount of civilians spawned
+            int amountToSpawn = (path.Waypoints.Length * (int) rate) * CIV_SPAWN_MOD;
+            int amountSpawned = 0;
+
+            // Position Civilians across the path
+            while (amountSpawned < amountToSpawn) {
+                Instantiate(Resources.Load("NPCs/Civilian/Civilian_car"));
+                amountSpawned++;
+                yield return null;
+            }
+            yield return 0;
+        }
+    }
+}
---
+++ Assets/Scenes/GameSceneManager.cs
@@ -0,0 +1,18 @@
+﻿using CivilianManager;
+using Heist;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSceneManager : Singleton<GameSceneManager>
+{
+    private const string NPC_PATH_TAG = "NPCPath";
+
+    protected GameSceneManager() { }
+
+    public IEnumerator SetUpGameScene() {
+        // TODO: Create heist here
+        Path npcPath = GameObject.FindGameObjectWithTag(NPC_PATH_TAG).GetComponent<Path>();
+        yield return StartCoroutine(CivilianSpawner.Instance.SpawnVehilcesOnPath(npcPath, CivilianSpawnRate.NORMAL));
+    }
+}
